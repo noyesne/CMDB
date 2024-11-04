8f5631a858996bb55f95faa02392c7accba1b74c
Luca Martinez <lucamartinez03@gmail.com>
2024-11-03 20:43:37 -0500

    CMDB-1-Create-GUI added button and centering

--- src/GUI/projectGUI.py
+++ src/GUI/projectGUI.py
@@ -15,6 +15,7 @@ filemenu.add_command(label='Exit',command=root.quit)
 editmenu = Menu(menu)
 menu.add_cascade(label='Edit',menu=editmenu)
 editmenu.add_command(label='Add Component')
+currentRow = 3
 
 Label(root, text='Part No.', width = 15, font=("Arial", 11)).grid(row = 1, column = 0, sticky = 'e', padx = 10, pady = 5)
 Label(root, text='Serial No.', width = 15, font=("Arial", 11)).grid(row = 2, column = 0, sticky = 'e', padx = 10, pady = 5)
@@ -23,21 +24,42 @@ E2 = Entry(root, width = 30)
 E1.grid(row = 1, column = 1, ipadx = 15, ipady = 5, sticky = 'w')
 E2.grid(row = 2, column = 1, ipadx = 15, ipady = 5, sticky = 'w')
 
-button1 = Button(root, text='Add Part', width=10, font=("Arial", 10))
-button2 = Button(root, text='Go', width=5, font=("Arial", 10), command=root.destroy)
+def addPart():
+    global currentRow
+    Label(root, text='Part No.', width = 15, font=("Arial", 11)).grid(row = currentRow, column = 0, sticky = 'e', padx = 10, pady = 5)
+    currentRow += 1
+    Label(root, text='Serial No.', width = 15, font=("Arial", 11)).grid(row = currentRow, column = 0, sticky = 'e', padx = 10, pady = 5)
+    E1 = Entry(root, width = 30)
+    E2 = Entry(root, width = 30)
+    currentRow -= 1
+    E1.grid(row = currentRow, column = 1, ipadx = 15, ipady = 5, sticky = 'w')
+    currentRow += 1
+    E2.grid(row = currentRow, column = 1, ipadx = 15, ipady = 5, sticky = 'w')
+    currentRow += 1
 
-Label(root, text=' ').grid(row=0, column=1) 
-Label(root, text=' ').grid(row=3, column=1) 
-Label(root, text=' ').grid(row=5, column=1)
+    button1.grid(row = currentRow, column = 0, columnspan = 2, pady = 10)
+    button2.grid(row = currentRow + 1, column = 0, columnspan = 2, pady = 10)
 
-button1.grid(row = 4, column = 0, columnspan = 2, pady = 10)
-button2.grid(row = 6, column = 0, columnspan = 2, pady = 10)
+    root.grid_rowconfigure(0, weight = 1)
+    root.grid_rowconfigure(currentRow + 2, weight = 1)
+
+button1 = Button(root, text='Add Part', width=10, font=("Arial", 10), command = addPart)
+button2 = Button(root, text='Go', width=5, font=("Arial", 10), command = root.destroy)
+
+button1.grid(row = 3, column = 0, columnspan = 2, pady = 10)
+button2.grid(row = 4, column = 0, columnspan = 2, pady = 10)
 
 root.grid_rowconfigure(0, weight = 1)
-root.grid_rowconfigure(7, weight = 1)
+root.grid_rowconfigure(currentRow + 2, weight = 1)
 root.grid_columnconfigure(0, weight = 1)
 root.grid_columnconfigure(1, weight = 1)
 root.grid_columnconfigure(2, weight = 1)
+
+#scrollbar = Scrollbar(root)
+#scrollbar.pack(side=RIGHT, fill=Y)
+
+#root.config(yscrollcommand=scrollbar.set)
+#scrollbar.config(command=root.yview)
 
 root.geometry("900x600")
 root.mainloop()
